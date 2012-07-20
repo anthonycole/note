3596202a7b016bd349961514042228b51854df1d
Anthony Cole <anthony@radiopicture.com.au>
2012-07-20 13:41:41 +1000

    Add widget to speaking at

--- sidebar.php
+++ sidebar.php
@@ -45,7 +45,7 @@
 			}).render().setUser('anthonycole').start();
 			</script>
 		</div>
-		<aside id="speaking">
+		<aside id="speaking" class="widget">
 			<h2>Speaking at</h2>
 			<a href="http://2012.sydney.wordcamp.org/" title="WordCamp Sydney July 21-22, 2012"><img alt="WordCamp Sydney July 21-22, 2012" src="http://2012.sydney.wordcamp.org/files/2012/05/WordCampSydneySpeaker-200px.png" /></a>
 		</aside>
