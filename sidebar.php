191e8b14615f47bf0860ce027f7c481ba7aee7d9
Anthony Cole <anthony@radiopicture.com.au>
2012-07-20 13:48:18 +1000

    Fix sidebar one last time

--- sidebar.php
+++ sidebar.php
@@ -44,7 +44,7 @@
 			  }
 			}).render().setUser('anthonycole').start();
 			</script>
-		</div>
+		</aside>
 		<aside id="speaking" class="widget">
 			<h2>Speaking at</h2>
 			<a href="http://2012.sydney.wordcamp.org/" title="WordCamp Sydney July 21-22, 2012"><img alt="WordCamp Sydney July 21-22, 2012" src="http://2012.sydney.wordcamp.org/files/2012/05/WordCampSydneySpeaker-200px.png" /></a>
