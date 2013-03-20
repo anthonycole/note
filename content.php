402730caa30d0a20e60aada073e5f61aec307db5
Anthony Cole <anthony@radiopicture.com.au>
2013-03-20 13:55:34 +1100

    Add proper timestamp

--- content.php
+++ content.php
@@ -5,7 +5,7 @@
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		
-		<p class="entry-meta"><span class="author"><?php the_author(); ?></span> &middot; <span class="published updated"><?php the_time(); ?></span></p>
+		<p class="entry-meta"><span class="author"><?php the_author(); ?></span> &middot; <span class="published updated"><?php the_time( get_option('date_format') . ' @ ' . get_option('time_format')); ?></span></p>
 		<section class="entry-content" >
 			<?php the_content(); ?>
 		</section>
