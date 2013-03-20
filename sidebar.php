c96039403598b635e075f4f468a217d31ef2ce06
Anthony Cole <anthony@radiopicture.com.au>
2013-03-20 13:56:06 +1100

    I can speak english

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,7 @@
 			?>
 			<article class="no-sidebar">
 				<h2>Not Configured</h2>
-				<p>Please configure your sidebar (Under Appearance -> Widgets</p>
+				<p>Please configure your sidebar (Under Appearance -> Widgets)</p>
 			</div>
 			<?php endif; ?>
 	</section>
